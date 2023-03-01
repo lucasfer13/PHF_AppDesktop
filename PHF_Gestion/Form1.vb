788a8815157a60306e351d38d0388fadd5ad37a7
BJJ <b.justo@insjoanbrudieu.cat>
2023-03-01 18:49:13 +0100

    Afegida pantalla gestio animals + tipus

---
+++ PHF_Gestion/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class TipusAnimal
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnAnimalBack_Click(sender As Object, e As EventArgs) Handles btnAnimalBack.Click
+        AnimalsGestio.show()
+        Me.Hide()
+    End Sub
+End Class
