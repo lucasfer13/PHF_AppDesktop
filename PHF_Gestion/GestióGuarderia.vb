249d9a31b53aaa6228ed4ce74cfa92cfb2af7e63
Albert Formenti <albertformenti@gmail.com>
2023-03-06 17:57:41 +0100

    Administracio Guarderia

---
+++ PHF_Gestion/GestióGuarderia.vb
@@ -0,0 +1,17 @@
+﻿Public Class GestióGuarderia
+    Private Sub GestióGuarderia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblMenuGestGuarderiesTitol_Click(sender As Object, e As EventArgs) Handles lblGestGuarderiaTitol.Click
+
+    End Sub
+
+    Private Sub lblGestGuarderiaNomGuarderia_Click(sender As Object, e As EventArgs) Handles lblGestGuarderiaNomGuarderia.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnGestGuarderiaModificar.Click
+
+    End Sub
+End Class
